d4f836cb47a792308ac96c1ab7812e69c50d9cd3
Aidan Dykstal <dykstal@mymail.mines.edu>
2020-02-27 12:05:36 -0700

    Added Autoencoder Analytic to Anomaly Detector

--- software/analyze/AnomalyDetector.py
+++ software/analyze/AnomalyDetector.py
@@ -13,6 +13,8 @@ import numpy as np
 # Analytic Functions
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import IsolationForest
+from pyod.models.auto_encoder import AutoEncoder
+import matplotlib.pyplot as plt
 
 # Class Declaration
 class AnomalyDetector:
@@ -29,7 +31,19 @@ class AnomalyDetector:
         self.M = {'methane_mixing_ratio_bias_corrected': np.array([1,2,3,4,5]),
                   'latitude': np.array([1,2,3,4,5]),
                   'longitude': np.array([1,2,3,4,5])}
-        self.y = self.M[self.config['model']['response']]
+        #self.y = self.M[self.config['model']['response']]
+        self.y = np.array([i + 1 for i in range(100)])
+
+    def plotAnomalyScores(self, anomalyScores):
+        '''
+        Plot a Histogram of Anomaly Scores from the Autoencoder Method. These Scores
+        will be Saved to the MATH582/software/analyze/static/images Directory. This
+        Plot should be Used as a Reference for Selecting the `anomalyScoreCutoff`
+        Hyperparameter for this Method.
+        '''
+        plt.hist(anomalyScores, bins = 'auto')
+        plt.title('Distribution of Autoencoder Anomaly Scores (Higher -> More Unusual)')
+        plt.savefig(os.path.join('software/analyze/static/images', 'anomalyScoresPlot.png'))
 
     def removeCommonData(self, spreadStatistic, threshold):
         '''
@@ -50,7 +64,7 @@ class AnomalyDetector:
 
         # Kill Data that are within 1 `spreadStatistic` of the Mean
         yMean = np.mean(self.y)
-        yStar = [] 
+        yStar = []
         idxList = []
         for idx in range(self.y.shape[0]):
             if np.abs(self.y[idx] - yMean) >= (threshold * ySpread):
@@ -64,7 +78,7 @@ class AnomalyDetector:
         '''
         # Find Model Hyperparameters
         hpMap = self.config['AnomalyDetector']['LocalOutlierFactorHyperparameters']
-        
+
         # Get the Thresholded Response
         yStar, idxList = self.removeCommonData(hpMap['spreadStatistic'], hpMap['threshold'])
         yStar = [[elem] for elem in yStar]
@@ -87,8 +101,8 @@ class AnomalyDetector:
         anomalyLonLatMap = {scores[i]: (self.M['longitude'][idx], self.M['latitude'][idx]) \
                             for idx in anomalyIdxList}
         sortedScores = sorted(scores)
-        anomaliesLonLatSorted = [anomalyLonLatMap[scores[i]] for i in range(len(sortedScores)) \
-                                 if scores[i] in anomalyLonLatMap]
+        anomaliesLonLatSorted = [anomalyLonLatMap[sortedScores[i]] for i in range(len(sortedScores)) \
+                                 if sortedScores[i] in anomalyLonLatMap]
         return anomaliesLonLatSorted
 
     def detectWithIsolationForest(self):
@@ -97,7 +111,7 @@ class AnomalyDetector:
         '''
         # Find Model Hyperparameters
         hpMap = self.config['AnomalyDetector']['IsolationForestHyperparameters']
-        
+
         # Get the Thresholded Response
         yStar, idxList = self.removeCommonData(hpMap['spreadStatistic'], hpMap['threshold'])
         yStar = [[elem] for elem in yStar]
@@ -117,8 +131,8 @@ class AnomalyDetector:
         anomalyLonLatMap = {scores[i]: (self.M['longitude'][idx], self.M['latitude'][idx]) \
                             for idx in anomalyIdxList}
         sortedScores = sorted(scores)
-        anomaliesLonLatSorted = [anomalyLonLatMap[scores[i]] for i in range(len(sortedScores)) \
-                                 if scores[i] in anomalyLonLatMap]
+        anomaliesLonLatSorted = [anomalyLonLatMap[sortedScores[i]] for i in range(len(sortedScores)) \
+                                 if sortedScores[i] in anomalyLonLatMap]
         return anomaliesLonLatSorted
 
     def detectWithAutoencoder(self):
@@ -127,8 +141,26 @@ class AnomalyDetector:
         '''
         # Find Model Hyperparameters
         hpMap = self.config['AnomalyDetector']['AutoencoderHyperparameters']
-        # TODO - COMING SOON :: I Found a Bug in my Code :<
-        return []
+
+        # Create and Fit the Autoencoder Model
+        AE = AutoEncoder(hidden_neurons = [1 for i in range(hpMap['depth'])])
+        AE.fit(self.y.reshape(-1, 1))
+
+        # Get & Plot Anomaly Scores for the Observations
+        anomalyScores = AE.decision_scores_
+        self.plotAnomalyScores(anomalyScores)
+
+        # Report the Lon/Lat Points Corresponding to the Anomalies
+        # in the Order of Decreasing Anomaly Score (i.e., the Most
+        # Anomalous Points are Shown First)
+        anomalyIdxList = [self.y[i] for i in range(self.y.shape[0]) \
+                          if anomalyScores[i] >= hpMap['anomalyScoreCutoff']]
+        anomalyLonLatMap = {anomalyScores[i]: (self.M['longitude'][idx], self.M['latitude'][idx]) \
+                            for idx in anomalyIdxList}
+        sortedScores = sorted(anomalyScores)
+        anomaliesLonLatSorted = [anomalyLonLatMap[sortedScores[i]] for i in range(len(sortedScores)) \
+                                 if sortedScores[i] in anomalyLonLatMap]
+        return anomaliesLonLatSorted
 
     def detectAnomalies(self):
         '''
